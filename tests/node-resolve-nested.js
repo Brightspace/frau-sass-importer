343c7e911f90ced9ec223d8739941cd1761ea483
Owen Smith <owen@omsmith.ca>
2015-11-18 10:12:07 -0500

    test(resolve) add integration test for nested node_modules

---
+++ tests/node-resolve-nested.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const
+	expect = require('chai').expect,
+	path = require('path'),
+	sass = require('node-sass');
+
+const frauImporter = require('../');
+
+describe('node-resolve-nested', () => {
+	let cwd;
+	before(() => {
+		cwd = process.cwd();
+
+		process.chdir(path.join(__dirname, 'node-resolve-nested'));
+	});
+	after(() => {
+		process.chdir(cwd);
+	});
+
+	it('should work', (done) => {
+		sass.render({
+			file: 'src/style.scss',
+			importer: frauImporter
+		}, (err, res) => {
+			expect(err).to.not.be.defined;
+
+			expect(res.css.toString('utf8').replace(/\s/g, '')).to.equal('h1{color:#00eeee;}');
+
+			done();
+		});
+	});
+});
