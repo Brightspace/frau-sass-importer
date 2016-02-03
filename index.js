7d8f9fc0ae814bacb4ce171a9de1b008ec2bebc0
Owen Smith <owen@omsmith.ca>
2016-02-03 11:27:10 -0500

    chore(style) conform to Brightspace ESLint
    Fixes #2

--- index.js
+++ index.js
@@ -1,73 +1,75 @@
+'use strict';
+
 var sass = require('node-sass'),
-    path = require('path'),
-    fs = require('fs'),
-    resolve = require('resolve');
+	path = require('path'),
+	fs = require('fs'),
+	resolve = require('resolve');
 
 var handledBaseFolderNames = {
-    'bower_components': 'bower_components',
-    'node_modules': 'node_modules'
+	'bower_components': 'bower_components',
+	'node_modules': 'node_modules'
 };
 
-function customImporter (url, prev, done) {
-    if (!endsWith(url, '.scss')) {
-        url += '.scss';
-    }
+function customImporter(url, prev, done) {
+	if (!endsWith(url, '.scss')) {
+		url += '.scss';
+	}
 
-    try {
-        var resolvedNpmPath = resolveNpm(url, prev);
-        if (resolvedNpmPath) {
-            return complete({ file: resolvedNpmPath }, done);
-        }
-    } catch (e) {}
+	try {
+		var resolvedNpmPath = resolveNpm(url, prev);
+		if (resolvedNpmPath) {
+			return complete({ file: resolvedNpmPath }, done);
+		}
+	} catch (e) { /* was not a node module */ }
 
-    var baseFolderName = url.split('/')[0];
+	var baseFolderName = url.split('/')[0];
 
-    if (handledBaseFolderNames[baseFolderName]) {
-        var result = findInParentDirSync(url, prev);
-        return complete(result, done);
-    } else {
-        return sass.NULL;
-    }
+	if (handledBaseFolderNames[baseFolderName]) {
+		var result = findInParentDirSync(url, prev);
+		return complete(result, done);
+	} else {
+		return sass.NULL;
+	}
 }
 
 function complete(result, done) {
-    if (isAsync(done)) {
-        done(result);
-        return;
-    }
-    return result;
+	if (isAsync(done)) {
+		done(result);
+		return;
+	}
+	return result;
 }
 
 function isAsync(doneCallback) {
-    return typeof doneCallback === 'function';
+	return typeof doneCallback === 'function';
 }
 
 function endsWith(str, suffix) {
-    return str && str.indexOf(suffix, str.length - suffix.length) !== -1;
+	return str && str.indexOf(suffix, str.length - suffix.length) !== -1;
 }
 
-function resolveNpm (id, referencingFile) {
-    return resolve.sync(id, {
-        basedir: path.dirname(referencingFile)
-    });
+function resolveNpm(id, referencingFile) {
+	return resolve.sync(id, {
+		basedir: path.dirname(referencingFile)
+	});
 }
 
 function findInParentDirSync(relativePath, startingDirPath) {
-    var dirToTry = path.join(startingDirPath, '..');
-    var pathToTry = path.join(dirToTry, relativePath);
+	var dirToTry = path.join(startingDirPath, '..');
+	var pathToTry = path.join(dirToTry, relativePath);
 
-    try {
-        fs.accessSync(pathToTry, fs.R_OK);
-        return {
-            file: pathToTry
-        };
-    } catch (err) {
-        if (pathToTry === ('/' + relativePath)) {
-            return new Error('File not found: ' + relativePath);
-        } else {
-            return findInParentDirSync(relativePath, dirToTry);
-        }
-    }
+	try {
+		fs.accessSync(pathToTry, fs.R_OK);
+		return {
+			file: pathToTry
+		};
+	} catch (err) {
+		if (pathToTry === ('/' + relativePath)) {
+			return new Error('File not found: ' + relativePath);
+		} else {
+			return findInParentDirSync(relativePath, dirToTry);
+		}
+	}
 }
 
 module.exports = customImporter;
--- tests/index.js
+++ tests/index.js
@@ -1,4 +1,4 @@
-/* global describe, it, beforeEach, afterEach */
+'use strict';
 
 var chai = require('chai'),
 	expect = chai.expect,
@@ -13,69 +13,69 @@ var importer = require('../');
 
 chai.use(sinonChai);
 
-describe('Custom importer', function () {
+describe('Custom importer', function() {
 	describe('async', function() {
-		describe('Unresolved path handling', function () {
-			it('should ignore relative path that cannot be resolved', function () {
+		describe('Unresolved path handling', function() {
+			it('should ignore relative path that cannot be resolved', function() {
 				var url = '../node_modules/module-name/some-sass.scss',
 					prev = '/parent/file/parent.scss';
 
-				var result = importer(url, prev, function (done) {
+				var result = importer(url, prev, function() {
 					expect.fail();
 				});
 
 				expect(result).to.equal(sass.NULL);
 			});
 
-			it('should ignore relative path starting with an unhandled base folder name that cannot be resolved', function () {
+			it('should ignore relative path starting with an unhandled base folder name that cannot be resolved', function() {
 				var url = 'foo_modules/module-name/some-sass.scss',
 					prev = '/parent/file/parent.scss';
 
-				var result = importer(url, prev, function (done) {
+				var result = importer(url, prev, function() {
 					expect.fail();
 				});
 
 				expect(result).to.equal(sass.NULL);
 			});
 
-			it('should ignore absolute path that canont be resolved', function () {
+			it('should ignore absolute path that canont be resolved', function() {
 				var url = '/node_modules/module-name/some-sass.scss',
 					prev = '/parent/file/parent.scss';
 
-				var result = importer(url, prev, function (done) {
+				var result = importer(url, prev, function() {
 					expect.fail();
 				});
 
 				expect(result).to.equal(sass.NULL);
 			});
 
-			it('should return an error when a node_modules path cannot be found', function (done) {
+			it('should return an error when a node_modules path cannot be found', function(done) {
 				var url = 'node_modules/module-name/some-sass.scss',
 					prev = '/parent/file/parent.scss';
 
-				importer(url, prev, function (result) {
+				importer(url, prev, function(result) {
 					expect(result).to.be.an('Error');
 					done();
 				});
 			});
 
-			it('should return an error when a bower_components path cannot be found', function (done) {
+			it('should return an error when a bower_components path cannot be found', function(done) {
 				var url = 'bower_components/component/component.scss',
 					prev = '/parent/file/parent.scss';
 
-				importer(url, prev, function (result) {
+				importer(url, prev, function(result) {
 					expect(result).to.be.an('Error');
 					done();
 				});
 			});
 		});
 
-		describe('npm path resolution', function () {
-			afterEach(function () {
+		describe('npm path resolution', function() {
+			afterEach(function() {
 				mockFs.restore();
 			});
 
-			it('should resolve scss reference using module name and file name in module root', function (done) {
+			it('should resolve scss reference using module name and file name in module root', function(done) {
 				var url = 'some-module/module.scss',
 					prev = 'app.scss';
 
@@ -83,7 +83,7 @@ describe('Custom importer', function () {
 					'node_modules/some-module/module.scss': ''
 				});
 
-				var returnValue = importer(url, prev, function (result) {
+				var returnValue = importer(url, prev, function(result) {
 					expect(result).to.have.property('file', path.resolve('node_modules/some-module/module.scss'));
 					done();
 				});
@@ -91,7 +91,7 @@ describe('Custom importer', function () {
 				expect(returnValue).to.be.undefined;
 			});
 
-			it('should resolve scss reference using module name and file name in module sub folder', function (done) {
+			it('should resolve scss reference using module name and file name in module sub folder', function(done) {
 				var url = 'some-module/src/component/module.scss',
 					prev = 'app.scss';
 
@@ -99,7 +99,7 @@ describe('Custom importer', function () {
 					'node_modules/some-module/src/component/module.scss': ''
 				});
 
-				var returnValue = importer(url, prev, function (result) {
+				var returnValue = importer(url, prev, function(result) {
 					expect(result).to.have.property('file', path.resolve('node_modules/some-module/src/component/module.scss'));
 					done();
 				});
@@ -107,7 +107,7 @@ describe('Custom importer', function () {
 				expect(returnValue).to.be.undefined;
 			});
 
-			it('should resolve scss reference using module name and file name without .scss extension', function (done) {
+			it('should resolve scss reference using module name and file name without .scss extension', function(done) {
 				var url = 'some-module/module',
 					prev = 'app.scss';
 
@@ -115,7 +115,7 @@ describe('Custom importer', function () {
 					'node_modules/some-module/module.scss': ''
 				});
 
-				var returnValue = importer(url, prev, function (result) {
+				var returnValue = importer(url, prev, function(result) {
 					expect(result).to.have.property('file', path.resolve('node_modules/some-module/module.scss'));
 					done();
 				});
@@ -124,82 +124,82 @@ describe('Custom importer', function () {
 			});
 		});
 
-		describe('node_modules and bower_components path resolution', function () {
+		describe('node_modules and bower_components path resolution', function() {
 			var sandbox,
 				resolvedPath;
 
-			beforeEach(function () {
+			beforeEach(function() {
 				sandbox = sinon.sandbox.create();
 				sandbox.stub(fs, 'accessSync', accessCheckHandler);
 				resolvedPath = null;
 			});
 
-			afterEach(function () {
+			afterEach(function() {
 				sandbox.restore();
 			});
 
-			function accessCheckHandler (path, mode) {
+			function accessCheckHandler(path) {
 				if (path === resolvedPath) {
 					return path;
 				}
 				throw new Error('File not found');
 			}
 
-			it('should find the file at the same level as the referencing file', function (done) {
+			it('should find the file at the same level as the referencing file', function(done) {
 				var url = 'node_modules/module/module.scss',
 					prev = '/project-root/main.scss';
 
 				resolvedPath = '/project-root/node_modules/module/module.scss';
 
-				importer(url, prev, function (result) {
+				importer(url, prev, function(result) {
 					expect(result).to.have.property('file', resolvedPath);
 					done();
 				});
 			});
 
-			it('should find the file one level up from referencing file', function (done) {
+			it('should find the file one level up from referencing file', function(done) {
 				var url = 'node_modules/module/module.scss',
 					prev = '/project-root/styles/main.scss';
 
 				resolvedPath = '/project-root/node_modules/module/module.scss';
 
-				importer(url, prev, function (result) {
+				importer(url, prev, function(result) {
 					expect(result).to.have.property('file', resolvedPath);
 					done();
 				});
 			});
 
-			it('should find the file two levels up from referencing file', function (done) {
+			it('should find the file two levels up from referencing file', function(done) {
 				var url = 'node_modules/module/module.scss',
 					prev = '/project-root/src/styles/main.scss';
 
 				resolvedPath = '/project-root/node_modules/module/module.scss';
 
-				importer(url, prev, function (result) {
+				importer(url, prev, function(result) {
 					expect(result).to.have.property('file', resolvedPath);
 					done();
 				});
 			});
 
-			it('should find the file when referenced without an extension', function (done) {
+			it('should find the file when referenced without an extension', function(done) {
 				var url = 'node_modules/module/module',
 					prev = '/project-root/main.scss';
 
 				resolvedPath = '/project-root/node_modules/module/module.scss';
 
-				importer(url, prev, function (result) {
+				importer(url, prev, function(result) {
 					expect(result).to.have.property('file', resolvedPath);
 					done();
 				});
 			});
 
-			it('should find the file when referenced from a bower component', function (done) {
+			it('should find the file when referenced from a bower component', function(done) {
 				var url = 'bower_components/component/component.scss',
 					prev = '/project-root/main.scss';
 
 				resolvedPath = '/project-root/bower_components/component/component.scss';
 
-				importer(url, prev, function (result) {
+				importer(url, prev, function(result) {
 					expect(result).to.have.property('file', resolvedPath);
 					done();
 				});
@@ -208,8 +208,8 @@ describe('Custom importer', function () {
 	});
 
 	describe('sync', function() {
-		describe('Unresolved path handling', function () {
-			it('should ignore relative path that cannot be resolved', function () {
+		describe('Unresolved path handling', function() {
+			it('should ignore relative path that cannot be resolved', function() {
 				var url = '../node_modules/module-name/some-sass.scss',
 					prev = '/parent/file/parent.scss';
 
@@ -217,7 +217,7 @@ describe('Custom importer', function () {
 				expect(result).to.equal(sass.NULL);
 			});
 
-			it('should ignore relative path starting with an unhandled base folder name that cannot be resolved', function () {
+			it('should ignore relative path starting with an unhandled base folder name that cannot be resolved', function() {
 				var url = 'foo_modules/module-name/some-sass.scss',
 					prev = '/parent/file/parent.scss';
 
@@ -225,7 +225,7 @@ describe('Custom importer', function () {
 				expect(result).to.equal(sass.NULL);
 			});
 
-			it('should ignore absolute path that canont be resolved', function () {
+			it('should ignore absolute path that canont be resolved', function() {
 				var url = '/node_modules/module-name/some-sass.scss',
 					prev = '/parent/file/parent.scss';
 
@@ -233,7 +233,7 @@ describe('Custom importer', function () {
 				expect(result).to.equal(sass.NULL);
 			});
 
-			it('should return an error when a node_modules path cannot be found', function () {
+			it('should return an error when a node_modules path cannot be found', function() {
 				var url = 'node_modules/module-name/some-sass.scss',
 					prev = '/parent/file/parent.scss';
 
@@ -241,7 +241,7 @@ describe('Custom importer', function () {
 				expect(result).to.be.an('Error');
 			});
 
-			it('should return an error when a bower_components path cannot be found', function () {
+			it('should return an error when a bower_components path cannot be found', function() {
 				var url = 'bower_components/component/component.scss',
 					prev = '/parent/file/parent.scss';
 
@@ -250,12 +250,12 @@ describe('Custom importer', function () {
 			});
 		});
 
-		describe('npm path resolution', function () {
-			afterEach(function () {
+		describe('npm path resolution', function() {
+			afterEach(function() {
 				mockFs.restore();
 			});
 
-			it('should resolve scss reference using module name and file name in module root', function () {
+			it('should resolve scss reference using module name and file name in module root', function() {
 				var url = 'some-module/module.scss',
 					prev = 'app.scss';
 
@@ -267,7 +267,7 @@ describe('Custom importer', function () {
 				expect(result).to.have.property('file', path.resolve('node_modules/some-module/module.scss'));
 			});
 
-			it('should resolve scss reference using module name and file name in module sub folder', function () {
+			it('should resolve scss reference using module name and file name in module sub folder', function() {
 				var url = 'some-module/src/component/module.scss',
 					prev = 'app.scss';
 
@@ -279,7 +279,7 @@ describe('Custom importer', function () {
 				expect(result).to.have.property('file', path.resolve('node_modules/some-module/src/component/module.scss'));
 			});
 
-			it('should resolve scss reference using module name and file name without .scss extension', function () {
+			it('should resolve scss reference using module name and file name without .scss extension', function() {
 				var url = 'some-module/module',
 					prev = 'app.scss';
 
@@ -292,28 +292,28 @@ describe('Custom importer', function () {
 			});
 		});
 
-		describe('node_modules and bower_components path resolution', function () {
+		describe('node_modules and bower_components path resolution', function() {
 			var sandbox,
 				resolvedPath;
 
-			beforeEach(function () {
+			beforeEach(function() {
 				sandbox = sinon.sandbox.create();
 				sandbox.stub(fs, 'accessSync', accessCheckHandler);
 				resolvedPath = null;
 			});
 
-			afterEach(function () {
+			afterEach(function() {
 				sandbox.restore();
 			});
 
-			function accessCheckHandler (path, mode) {
+			function accessCheckHandler(path) {
 				if (path === resolvedPath) {
 					return path;
 				}
 				throw new Error('File not found');
 			}
 
-			it('should find the file at the same level as the referencing file', function () {
+			it('should find the file at the same level as the referencing file', function() {
 				var url = 'node_modules/module/module.scss',
 					prev = '/project-root/main.scss';
 
@@ -323,7 +323,7 @@ describe('Custom importer', function () {
 				expect(result).to.have.property('file', resolvedPath);
 			});
 
-			it('should find the file one level up from referencing file', function () {
+			it('should find the file one level up from referencing file', function() {
 				var url = 'node_modules/module/module.scss',
 					prev = '/project-root/styles/main.scss';
 
@@ -333,7 +333,7 @@ describe('Custom importer', function () {
 				expect(result).to.have.property('file', resolvedPath);
 			});
 
-			it('should find the file two levels up from referencing file', function () {
+			it('should find the file two levels up from referencing file', function() {
 				var url = 'node_modules/module/module.scss',
 					prev = '/project-root/src/styles/main.scss';
 
@@ -343,7 +343,7 @@ describe('Custom importer', function () {
 				expect(result).to.have.property('file', resolvedPath);
 			});
 
-			it('should find the file when referenced without an extension', function () {
+			it('should find the file when referenced without an extension', function() {
 				var url = 'node_modules/module/module',
 					prev = '/project-root/main.scss';
 
@@ -353,7 +353,7 @@ describe('Custom importer', function () {
 				expect(result).to.have.property('file', resolvedPath);
 			});
 
-			it('should find the file when referenced from a bower component', function () {
+			it('should find the file when referenced from a bower component', function() {
 				var url = 'bower_components/component/component.scss',
 					prev = '/project-root/main.scss';
 
